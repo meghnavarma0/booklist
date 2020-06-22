03aecd7e4b663680326008c13a48aa51debd5758
meghnavarma0 <meghnavarma0@gmail.com>
2020-06-22 20:12:29 +0530

    completed BookDetails component

--- booklist/src/App.js
+++ booklist/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import BookContextProvider from './contexts/BookContext';
 import Navbar from './components/Navbar';
+import BookList from './components/BookList';
 
 function App() {
 	return (
 		<div className='App'>
 			<BookContextProvider>
 				<Navbar />
+				<BookList />
 			</BookContextProvider>
 		</div>
 	);
